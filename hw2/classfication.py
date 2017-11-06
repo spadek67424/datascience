355388091e8b3afe5347a500db77b26e8c4f0f11
spadek67424 <spadek67424@gmail.com>
2017-11-06 14:05:21 +0800

    all

--- hw2/classfication.py
+++ hw2/classfication.py
@@ -2,6 +2,11 @@ import numpy as np
 import sys
 import os
 from sklearn import linear_model,tree
+from sklearn.svm import LinearSVC
+from sklearn.neural_network import MLPClassifier
+'''
+remember to tune the parameter
+'''
 
 def traindata(arg):
 	data=np.genfromtxt(arg,delimiter=",")
@@ -17,17 +22,26 @@ def regression (trainx,trainy,testx):
 	logreg = linear_model.LogisticRegression(C=1e5)
 	logreg.fit(trainx,trainy)
 	testy=logreg.predict(testx)
-	print(testy.shape)
-	print(logreg.score(trainx,trainy))
+	print("R=",logreg.score(trainx,trainy))
 	return testy
 def des(trainx,trainy,testx):
 	clf=tree.DecisionTreeClassifier()
 	clf.fit(trainx,trainy)
 	testy=clf.predict(testx)
-	print(clf.score(trainx,trainy))
+	print("D=",clf.score(trainx,trainy))
 	return testy
-
-
+def svm(trainx,trainy,testx):
+	sv=LinearSVC()
+	sv.fit(trainx,trainy)
+	testy=sv.predict(testx)
+	print("S=",sv.score(trainx,trainy))
+	return testy
+def NN(trainx,trainy,testx):
+	nn= MLPClassifier(hidden_layer_sizes=(100,100,100),max_iter=400,learning_rate_init=0.001)
+	nn.fit(trainx,trainy)
+	print("N=",nn.score(trainx,trainy))
+	testy=nn.predict(testx)
+	return testy
 
 if __name__ == '__main__':
 	arg=sys.argv
@@ -38,7 +52,7 @@ if __name__ == '__main__':
 	if "D" in arg[1]:
 		des(trainx,trainy,testx)
 	if "S" in arg[1]:
-		regression(trainx,trainy,testx)
+		svm(trainx,trainy,testx)
 	if "N" in arg[1]:
-		des(trainx,trainy,testx)
+		NN(trainx,trainy,testx)
 	
