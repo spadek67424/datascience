91cc00719da2e76bb58c5d61d86b87a74e6b5b6e
spadek67424 <spadek67424@gmail.com>
2017-12-01 22:05:01 +0800

    try model on web

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -11,6 +11,13 @@ def unpickle(file):
 	with open(file, 'rb') as fo:
 		dict = pickle.load(fo, encoding='bytes')
 	return dict
+def norm(nonx):
+	nonx=nonx/255
+	mean=np.mean(nonx)
+	std=np.std(nonx)
+	fex=(nonx-mean)/std
+	return fex
+
 if __name__=='__main__':
 	data=list()
 	label=list()
@@ -39,32 +46,60 @@ if __name__=='__main__':
 	testx=dicts[b'data']
 	testy=dicts[b'labels']
 	testy=np.array(testy)
+	trainx=norm(trainx.reshape((60000,3072)))
+	testx=norm(testx.reshape(10000,3072))
 	trainx=trainx.reshape((60000,32,32,3))
 	trainy=trainy.reshape(60000,1)
 	trainy=np_utils.to_categorical(trainy)
 	testx=testx.reshape((10000,32,32,3))
 	testy=testy.reshape(10000,1)
 	test=np_utils.to_categorical(testy)
-	print(trainy.shape)
-	print(' ')
-	print(testx.shape)
-	print('')
-	print(testy.shape)
-
+	'''
 	model = Sequential()
 	model.add(Conv2D(64,(5,5),input_shape=(32,32,3),activation='relu'));
 	model.add(MaxPooling2D((2,2),strides=2))
-	'''
-	model.add(Conv1D(128,20,activation='relu'));
-	model.add(Conv1D(192,15,activation='relu'));
-	model.add(Dense(units=1000,activation='relu'))
-	model.add(Dense(units=10,activation='relu'))
-	'''
+	
 	model.add(Flatten())
 	model.add(Dense(units=10,activation='softmax'))
 	
 	model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 	model.fit(trainx,trainy,validation_split=0,shuffle=True,batch_size=100,epochs=50)
 	model.evaluate(testx,testy);
-	
+	'''
+
+	'''
+	try model
+	'''
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), activation='relu', padding='same'))
+	model.add(Dropout(0.2))
+	model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+	model.add(Dropout(0.2))
+	model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+	model.add(Dropout(0.2))
+	model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Flatten())
+	model.add(Dropout(0.2))
+	model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
+	model.add(Dropout(0.2))
+	model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+	model.add(Dropout(0.2))
+	model.add(Dense(num_classes, activation='softmax'))
+
+	epochs = 25
+	lrate = 0.01
+	decay = lrate/epochs
+	sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+	model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+	print(model.summary())
+
+	model.fit(trainx, trainy, validation_data=(testx, testy), epochs=epochs, batch_size=32)
+	# Final evaluation of the model
+	scores = model.evaluate(testx, testy, verbose=0)
+	print("Accuracy: %.2f%%" % (scores[1]*100))
 	#print(dat.a)
