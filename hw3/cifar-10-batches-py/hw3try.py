753a0394ad268e5c791125126414a7f38aa4b4ab
spadek67424 <spadek67424@gmail.com>
2017-12-04 22:13:54 +0800

    try different model

--- hw3/cifar-10-batches-py/hw3try.py
+++ hw3/cifar-10-batches-py/hw3try.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from keras.datasets import cifar10
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D,AveragePooling2D
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import np_utils
 from keras.callbacks import History
@@ -12,18 +12,29 @@ import numpy as np
 import os
 import sys
 import pickle
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+
+
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 1.0
+set_session(tf.Session(config=config))
 '''
 try et model
 '''
 channels = 3
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+def unpickle(file):
+	import pickle
+	with open(file, 'rb') as fo:
+		dict = pickle.load(fo, encoding='bytes')
+	return dict
 def unpickle(file):  
     with open(file, 'rb') as fo:
         dict = pickle.load(fo,encoding='bytes')
     return dict
 
 def convert_images(raw_images):
-    raw = np.array(raw_images, dtype = float)
+    raw = np.array(raw_images, dtype = float)/255.0
     images = raw.reshape([-1, channels, size, size])
     images = images.transpose([0, 2, 3, 1])
     return images
@@ -37,7 +48,7 @@ def load_data(file):
 
 def get_test_data():
     images, labels = load_data("test_batch")
-    return images, labels, np_utils.to_categorical(labels, num_classes)
+    return images, labels, np_utils.to_categorical(labels, 10)
 
 def get_train_data():
     images = np.zeros(shape = [50000, size, size, channels], dtype = float)
@@ -52,25 +63,20 @@ def get_train_data():
         start = end
 
     return images, labels, np_utils.to_categorical(labels,10)
+def norm(nonx):
+	nonx=nonx/255
+
+	mean=np.mean(nonx)
+	std=np.std(nonx)
+	fex=(nonx-mean)/std
+	return fex
 
 if __name__=='__main__':
-	data=list()
-	label=list()
 	
+	trainx,labelx,trainy=get_train_data()
+	testx,labely,testy=get_test_data()
 
-	
-	trainx , labels, trainy = get_train_data()
-	#plt.imshow(trainx[0])
-	
-	print(trainx[0])
-	print(x_train[0])
-	plt.imshow(x_train[0])
-	trainy=trainy.reshape(50000,1)
-	trainy=np_utils.to_categorical(trainy,10)
-	testx=testx.reshape((10000,32,32,3))
-	testy=testy.reshape(10000,1)
-	testy=np_utils.to_categorical(testy,10)
-	
+
 
 
 	'''
@@ -89,7 +95,7 @@ if __name__=='__main__':
 	'''
 	try model
 	'''
-	'''
+	
 
 	model = Sequential()
 
@@ -114,21 +120,14 @@ if __name__=='__main__':
 	model.add(AveragePooling2D(pool_size=(2, 2)))  
 	model.add(Dropout(0.25))  
 	  
-	model.add(Conv2D(256, (3, 3), padding='same'))  
-	model.add(Activation('relu'))  
-	model.add(Conv2D(256, (1, 1)))  
-	model.add(Activation('relu'))  
-	model.add(AveragePooling2D(pool_size=(2, 2)))  
-	model.add(Dropout(0.25))  
-	  
 	model.add(Flatten())  
 	model.add(Dense(512))  
 	model.add(Activation('relu'))  
 	model.add(Dropout(0.5))  
-	model.add(Dense(num_classes))  
+	model.add(Dense(10))  
 	model.add(Activation('softmax'))  
-	'''
-	'''
+	
+
 	datagen = ImageDataGenerator(
 		featurewise_center=False,  # set input mean to 0 over the dataset
         samplewise_center=False,  # set each sample mean to 0
@@ -141,11 +140,11 @@ if __name__=='__main__':
         horizontal_flip=True,  # randomly flip images
         vertical_flip=False)  # randomly flip images
 
-	epochs = 300
+	epochs = 400
 	lrate = 0.01
 	decay = lrate/epochs
-	opt = keras.optimizers.Adam(lr=0.000001)
-	#opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+	opt = keras.optimizers.Adam(decay=0.000001)
+	#opt = keras.optimizers.rmsprop(lr=0.001, decay=1e-6)
 
 	# Let's train the model using RMSprop
 	model.compile(loss='categorical_crossentropy',
@@ -158,7 +157,7 @@ if __name__=='__main__':
                         epochs=epochs,
                         validation_data=(testx, testy),
                         steps_per_epoch=50000//32)
-	'''
+	
 	'''
 	model.fit(trainx, trainy,
               batch_size=32,
@@ -167,7 +166,7 @@ if __name__=='__main__':
               shuffle=True)
 	'''
 
-	'''
+	
 	# Final evaluation of the model
 	scores = model.evaluate(testx, testy, verbose=1)
 	model.save('model.h5')
@@ -176,5 +175,5 @@ if __name__=='__main__':
 	plt.plot(his.history['acc'])
 	plt.plot(his.history['val_acc'])
 	plt.savefig('one.png')
-	'''
+	
 	#print(dat.a)
